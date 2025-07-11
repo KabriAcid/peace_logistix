253ff522b95f3f9fbf7398726b8922e3ece4294f
KabriAcid <kabriacid01@gmail.com>
2025-07-11 22:21:59 +0100

    Added all the CDN

--- components/header.php
+++ components/header.php
@@ -5,13 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Peace Logistix - Dashboard</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap-grid.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap-utilities.min.css" rel="stylesheet">
-
-    <link rel="stylesheet" href="../assets/css/toasted.css">
     <link rel="shortcut icon" href="../favicon.png" type="image/x-icon">
+    
+    <!-- Icons stylesheets -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Creative Bootstrap -->
+    <link rel="stylesheet" href="../assets/css/soft-design-system-pro.min.css">
+
+    <!-- Toasted library -->
+    <link rel="stylesheet" href="../assets/css/toasted.css">
+    
+    <!-- Custom stylesheets -->
     <link rel="stylesheet" href="../assets/css/dashboard.css">
     <link rel="stylesheet" href="../assets/css/style.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
 </head>
